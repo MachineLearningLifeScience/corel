2d055dd70dd224719349460b7677b18b7fe11a29
Simon Bartels <bartels@di.ku.dk>
2023-08-17 14:06:08 +0200

    refactored pareto explorer for better inheritance

--- src/corel/optimization/pareto_frontier_explorer.py
+++ src/corel/optimization/pareto_frontier_explorer.py
@@ -13,31 +13,6 @@ This optimization algorithm imitates LamBO. We just explore all single site muta
 """
 
 
-def make_pareto_frontier_explorer(dataset=None):
-    if dataset is None:
-        observations_handle = lambda ac: tf.concat([ac._model._models[i].dataset.observations for i in range(len(ac._model._models))], axis=-1)
-        inputs_handle = lambda ac: ac._model._models[0].dataset.query_points
-    else:
-        # TODO: THIS DOES NOT WORK!
-        raise NotImplementedError("this does not work! I need the most recent data!")
-        observations_handle = lambda ac: dataset
-
-    def pareto_frontier_explorer(search_space: SearchSpaceType, acquisition_function) -> tf.Tensor:
-        # TODO: adapt for batch setting
-        # TODO: test whether it's necessary to change the sign of the observations
-        # seems not
-        _, idx = non_dominated(observations_handle(acquisition_function))
-        proposal_seqs = inputs_handle(acquisition_function)[idx]
-        selected_seq, best_val = _get_best_of_single_site_mutations(proposal_seqs[:1, ...], acquisition_function)
-        for i in range(1, len(proposal_seqs)):
-            s, v = _get_best_of_single_site_mutations(proposal_seqs[i:i+1, ...], acquisition_function)
-            if v > best_val:
-                selected_seq = s
-                best_val = v
-        return selected_seq
-    return pareto_frontier_explorer
-
-
 def _get_best_of_single_site_mutations(seq, acquisition_function):
     AA = acquisition_function._model._models[0].AA  # get from ac
     atom = _seq_to_atom(seq, AA)
@@ -59,6 +34,31 @@ def _get_best_of_single_site_mutations(seq, acquisition_function):
                 val = val_
         seq_[0, l] = seq[0, l].numpy()
     return seq, val
+
+
+def make_pareto_frontier_explorer(dataset=None, get_best_of_single_site_mutations=_get_best_of_single_site_mutations):
+    if dataset is None:
+        observations_handle = lambda ac: tf.concat([ac._model._models[i].dataset.observations for i in range(len(ac._model._models))], axis=-1)
+        inputs_handle = lambda ac: ac._model._models[0].dataset.query_points
+    else:
+        # TODO: THIS DOES NOT WORK!
+        raise NotImplementedError("this does not work! I need the most recent data!")
+        observations_handle = lambda ac: dataset
+
+    def pareto_frontier_explorer(search_space: SearchSpaceType, acquisition_function) -> tf.Tensor:
+        # TODO: adapt for batch setting
+        # TODO: test whether it's necessary to change the sign of the observations
+        # seems not
+        _, idx = non_dominated(observations_handle(acquisition_function))
+        proposal_seqs = inputs_handle(acquisition_function)[idx]
+        selected_seq, best_val = get_best_of_single_site_mutations(proposal_seqs[:1, ...], acquisition_function)
+        for i in range(1, len(proposal_seqs)):
+            s, v = get_best_of_single_site_mutations(proposal_seqs[i:i+1, ...], acquisition_function)
+            if v > best_val:
+                selected_seq = s
+                best_val = v
+        return selected_seq
+    return pareto_frontier_explorer
 
 
 def _seq_to_atom(x, AA):
