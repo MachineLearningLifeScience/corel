1fc311b7ccd43daade049d9144765fb8c7ab7d91
Simon Bartels <bartels@di.ku.dk>
2023-08-22 16:14:44 +0200

    adapted kbest to needs in pareto frontier explorer

--- src/corel/util/k_best.py
+++ src/corel/util/k_best.py
@@ -1,19 +1,25 @@
 __author__ = 'Simon Bartels'
+
+from typing import Callable
+
 import numpy as np
 
 
 class KeepKBest:
-    def __init__(self, k: int):
+    def __init__(self, k: int, copy: Callable = lambda x: x):
         """
         Keeps track of k-best values.
         :param k:
             the number of values to keep track of
+        :param copy:
+            a function that makes a copy of an element if necessary
         """
         self.k = k
+        self.copy = copy
         self.vals = -np.infty * np.ones(k)
         self.elements = np.array(k)
 
-    def new_val(self, val, element):
+    def new_val(self, val, element) -> bool:
         if val > self.vals[-1]:
             idx = np.argmax(val > self.vals)
             #self.vals = np.stack([self.vals[:idx], [val], self.vals[idx:-1]])
@@ -22,7 +28,9 @@ class KeepKBest:
             self.vals[idx+1:] = self.vals[idx:-1]
             self.vals[idx] = val
             self.elements[idx+1:] = self.elements[idx:-1]
-            self.elements[idx] = element
+            self.elements[idx] = self.copy(element)
+            return True
+        return False
 
     def get(self):
         assert(self.vals.shape[0] == self.k)
