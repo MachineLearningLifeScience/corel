edcc905cbab2286322dbd9770dfd6cce6c07cdb8
Simon Bartels <bartels@di.ku.dk>
2023-09-13 18:13:52 +0200

    made CBAS VAE more flexible

--- src/corel/weightings/vae/vae_weighting.py
+++ src/corel/weightings/vae/vae_weighting.py
@@ -8,15 +8,16 @@ from corel.weightings.vae.cbas_vae_wrapper import CBASVAEWrapper
 
 
 class VAEWeighting(AbstractWeighting):
-    def __init__(self):
-        raise NotImplementedError("Make sure implementation is correct")
-        vae = CBASVAEWrapper(AA=20, L=237)
+    def __init__(self, AA, L, prefix):
+        #raise NotImplementedError("Make sure implementation is correct")
+        vae = CBASVAEWrapper(AA=AA, L=L, prefix=prefix)
         # TODO: average over more distributions
-        self.p0 = vae.decode(tf.zeros([1, 20]))
+        latent_dim = vae.vae.latentDim_
+        self.p0 = vae.decode(tf.zeros([1, latent_dim]))
 
     def expectation(self, p):
         if p.dtype.is_integer:
-            raise NotImplementedError("obtain number of amino acids and make sure that permutation is correct")
+            #raise NotImplementedError("obtain number of amino acids and make sure that permutation is correct")
             p_ = tf.one_hot(p, 20, dtype=default_float(), axis=-1)
         else:
             p_ = p
