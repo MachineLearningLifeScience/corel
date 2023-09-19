f27f615be06a78ea0a68d49fe201801032d21341
Richard Michael <richard.michael@di.ku.dk>
2023-09-19 09:09:22 +0200

    Resolve "Observer implementation correction: seeds"

--- experiments/lambo_reference_rfp_experiment.sh
+++ experiments/lambo_reference_rfp_experiment.sh
@@ -2,5 +2,11 @@
 conda activate poli__lambo
 
 for i in $(seq 0 9); do
-    python scripts/black_box_opt.py optimizer=lambo optimizer.encoder_obj=mlm task=poli_rfp_internal tokenizer=protein surrogate=multi_task_exact_gp acquisition=ehvi trial_id=${i}
+    python scripts/black_box_opt.py optimizer=lambo optimizer.encoder_obj=mlm task=poli_rfp_internal tokenizer=protein surrogate=multi_task_exact_gp acquisition=ehvi trial_id=${i} optimizer.num_rounds=100
 done
+
+
+
+# Require task specifications on poli side to be included in hydra search path
+
+python scripts/black_box_opt.py optimizer=lambo optimizer.encoder_obj=mlm task=rfp_internal tokenizer=protein surrogate=multi_task_exact_gp acquisition=ehvi trial_id=2 optimizer.num_rounds=100 'hydra.searchpath=[file:///Users/rcml/poli/src/poli/objective_repository/foldx_rfp_lambo/]'
