7d54b8872a19da734269b21e56ddf81b425a7469
Richard Michael <r.michael@posteo.net>
2023-09-26 15:31:07 +0200

    lambo script without special config path

--- experiments/lambo_reference_rfp_experiment.sh
+++ experiments/lambo_reference_rfp_experiment.sh
@@ -8,5 +8,6 @@ done
 
 
 # Require task specifications on poli side to be included in hydra search path
-
-python scripts/black_box_opt.py optimizer=lambo optimizer.encoder_obj=mlm task=rfp_internal tokenizer=protein surrogate=multi_task_exact_gp acquisition=ehvi trial_id=2 optimizer.num_rounds=100 'hydra.searchpath=[file:///Users/rcml/poli/src/poli/objective_repository/foldx_rfp_lambo/]'
+python scripts/black_box_opt.py optimizer=lambo optimizer.encoder_obj=mlm task=poli_rfp_reference tokenizer=protein surrogate=multi_task_exact_gp acquisition=ehvi trial_id=2 optimizer.num_rounds=100
+# Benchmark Poli Task
+python scripts/black_box_opt.py optimizer=lambo optimizer.encoder_obj=mlm task=poli_rfp tokenizer=protein surrogate=multi_task_exact_gp acquisition=ehvi trial_id=2 optimizer.num_rounds=100 # 'hydra.searchpath=[file:///Users/rcml/poli/src/poli/objective_repository/foldx_rfp_lambo/]'
