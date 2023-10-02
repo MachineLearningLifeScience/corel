e122d49f3fe9e3f2712a05bb06b83e880835fdff
Miguel González Duque <miguelgondu@gmail.com>
2023-10-02 14:55:09 +0200

    Add preprocessing for the small molecules experiment

---
+++ src/corel/weightings/vae/small_molecules/vae_selfies.py
@@ -0,0 +1,5 @@
+"""This module implements a VAE for SELFIES representations of molecules.
+
+Using the ZINC250k dataset, we train a Variational Autoencoder
+that decodes to a categorical distribution.
+"""
