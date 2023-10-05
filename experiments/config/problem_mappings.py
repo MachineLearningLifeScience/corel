3a2bdfdeee923178c4832ada8db5394d83e0e48a
Simon Bartels <bartels@di.ku.dk>
2023-10-05 17:42:05 +0200

    updated config

--- experiments/config/problem_mappings.py
+++ experiments/config/problem_mappings.py
@@ -3,5 +3,5 @@ __author__ = 'Simon Bartels'
 
 # contains for each problem name the corresponding HMM
 hmm_problem_model_mapping = {
-    "FOLDX_RFP": "./assets/hmms/rfp.hmm"
+    "foldx_rfp_lambo": "./assets/hmms/rfp.hmm"
 }
