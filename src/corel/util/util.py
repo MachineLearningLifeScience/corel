6b5a96721b6d3a32075d34fa2ad3d5349f5b37e4
Miguel González Duque <miguelgondu@gmail.com>
2024-01-04 15:02:05 +0100

    Starts implementing the experiment using baselines

--- src/corel/util/util.py
+++ src/corel/util/util.py
@@ -14,10 +14,7 @@ def get_amino_acid_integer_mapping_from_info(setup_info: ProblemSetupInformation
     assert(PADDING_SYMBOL_INDEX == 0)
     return {alphabet[i]: i+offset for i in range(len(alphabet))}
 
-
-def transform_string_sequences_to_integer_arrays(train_x_, L, amino_acid_integer_mapping):
-    # the assertion below is only valid if sequences are not aligned
-    #assert(PADDING_SYMBOL_INDEX not in amino_acid_integer_mapping.values())
+def _transform_string_sequences_to_integer_arrays(train_x_, L, amino_acid_integer_mapping) -> np.ndarray:
     train_x = np.zeros([len(train_x_), L], dtype=int)
     assert(PADDING_SYMBOL_INDEX == 0)
     for i in range(len(train_x_)):
@@ -25,8 +22,23 @@ def transform_string_sequences_to_integer_arrays(train_x_, L, amino_acid_integer
         len_seq = len(seq)
         train_x[i, :len_seq] = np.array([amino_acid_integer_mapping.get(a, PADDING_SYMBOL_INDEX) for a in seq]) # element '' not in alphabet and defaults to PADDING
         train_x[i, len_seq:] = PADDING_SYMBOL_INDEX
+    
+    return train_x
+
+def transform_string_sequences_to_integer_arrays(train_x_, L, amino_acid_integer_mapping) -> tf.Tensor:
+    # the assertion below is only valid if sequences are not aligned
+    #assert(PADDING_SYMBOL_INDEX not in amino_acid_integer_mapping.values())
+    train_x = _transform_string_sequences_to_integer_arrays(train_x_, L, amino_acid_integer_mapping)
     return tf.constant(train_x)
 
+def transform_string_sequences_to_string_arrays(train_x_: np.ndarray, L: int) -> np.ndarray:
+    train_x = np.zeros([len(train_x_), L], dtype=str)
+    for i, sequence in enumerate(train_x_):
+        len_seq = len(sequence)
+        train_x[i, :len_seq] = list(sequence)
+        train_x[i, len_seq:] = ""
+    
+    return train_x
 
 def handle_batch_shape(X: tf.Tensor) -> tf.Tensor:
     if len(X.shape) == 4:
