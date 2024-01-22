56cbfa06a752b5275f0cc3e58e428e9fd2b2a013
Richard Michael <r.michael@posteo.net>
2024-01-22 12:22:26 +0100

    add RFP SLURM config runfiles and scheduling scripts

--- slurm/slurm_schedule_rfp_corel_experiments.sh
+++ slurm/slurm_schedule_rfp_corel_experiments.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=RFP_LAMBO
+#SBATCH --job-name=RFP_COREL
 #SBATCH -p boomsma
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=6
 #SBATCH --mem-per-cpu=8G
-#SBATCH --array=9-26
+#SBATCH --array=0-26
 #SBATCH --time=3-12:00:00
 
 HOME_DIR=/home/pcq275/
-LAMBO_DIR=${HOME_DIR}/lambo/
+COREL_DIR=${HOME_DIR}/corel/
 
 
 CONDA_BASE=$(conda info --base)
@@ -21,7 +21,7 @@ if [ $(cat ${CONDA_PREFIX}/etc/conda/activate.d/env_vars.sh | wc -l) == 0 ]; the
     source $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
 fi
 
-CONFIG=${COREL_DIR}slurm/rfp_lambo_config.txt
+CONFIG=${COREL_DIR}slurm/rfp_config.txt
 
 seed=$(awk -v ArrayID=${SLURM_ARRAY_TASK_ID} '$1==ArrayID {print $2}' ${CONFIG})
 batchsize=$(awk -v ArrayID=${SLURM_ARRAY_TASK_ID} '$1==ArrayID {print $3}' ${CONFIG})
@@ -30,8 +30,8 @@ asset_dir=$(awk -v ArrayID=${SLURM_ARRAY_TASK_ID} '$1==ArrayID {print $5}' ${CON
 n_start=$(awk -v ArrayID=${SLURM_ARRAY_TASK_ID} '$1==ArrayID {print $6}' ${CONFIG})
 iterations=$(awk -v ArrayID=${SLURM_ARRAY_TASK_ID} '$1==ArrayID {print $7}' ${CONFIG})
 
-conda activate poli__lambo
-echo "python ${LAMBO_DIR}/scripts/black_box_opt.py optimizer=lambo optimizer.encoder_obj=mlm task=${task} tokenizer=protein surrogate=multi_task_exact_gp acquisition=nehvi trial_id=${seed} optimizer.num_rounds=${iterations} task.num_start_examples=${n_start} +task.data_path=${asset_dir}"
-python ${LAMBO_DIR}/scripts/black_box_opt.py optimizer=lambo optimizer.encoder_obj=mlm task=${task} tokenizer=protein surrogate=multi_task_exact_gp acquisition=nehvi trial_id=${seed} optimizer.num_rounds=${iterations} task.num_start_examples=${n_start} +task.data_path=${asset_dir}
+conda activate corel-env
+echo "python ${COREL_DIR}/experiments/run_cold_warm_start_experiments_rfp_bo.py -b ${batchsize} -s ${seed} -p ${task} -n ${n_start} -m ${NUM_ITERATIONS}"
+python ${COREL_DIR}/experiments/run_cold_warm_start_experiments_rfp_bo.py -b ${batchsize} -s ${seed} -p ${task} -n ${n_start} -m ${NUM_ITERATIONS}
 
 exit 0
