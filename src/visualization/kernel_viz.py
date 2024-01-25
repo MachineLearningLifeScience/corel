6db8098261293f08421fe2761fea27864204644a
Richard Michael <r.michael@posteo.net>
2024-01-25 20:15:39 +0100

    refactor kernel values computation not in visualization script

--- src/visualization/kernel_viz.py
+++ src/visualization/kernel_viz.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 from gpflow.kernels import Kernel
+from matplotlib.ticker import FixedFormatter, FixedLocator
 
 
 def plotkernelsample_in_R(k: Kernel, ax, xmin: float=-3., xmax: float=3.):
@@ -44,31 +45,30 @@ def plotlatentspace_lvm(k: Kernel, lvm: object, ax, xmin: float=-2., xmax: float
     ax.set_title(f"2D Latent Space\n {k.__class__.__name__}", fontsize=20)
     return img
 
-def plotlatentspace_lvm_refpoint(k: Kernel, lvm: object, ax, xmin: float=-2., xmax: float=2., stepsize=0.01, ref_point=(0,0), vmin=0., vmax=1., cmap="viridis"):
-    xxyy = tf.convert_to_tensor(np.mgrid[xmin:xmax:stepsize, xmin:xmax:stepsize].reshape(2, -1).T)
-    if not isinstance(ref_point, tf.Tensor): # convert tuple to tensor
-        ref_point = tf.cast(tf.convert_to_tensor(ref_point)[None,:], tf.float64)
-    if "hellinger" not in k.__class__.__name__.lower(): # compare euclidean kernels directly in latent space
-        # mean reduction across positions # CASE: Matern
-        values = k(xxyy, ref_point)
-    elif "weighted" not in k.__class__.__name__.lower(): # CASE: base HK
-        decoded_latent = lvm.vae.decode(xxyy) # TODO: this is for the 2D case
-        ps = tf.reshape(tf.squeeze(decoded_latent), shape=(len(decoded_latent), decoded_latent.shape[-1]*decoded_latent.shape[-2]))
-        decoded_reference = lvm.vae.decode(ref_point.reshape(1, 2)) 
-        ps_ref = tf.reshape(tf.squeeze(decoded_reference), shape=(1, decoded_reference.shape[-1]*decoded_reference.shape[-2]))
-        values = k(ps, ps_ref)
-    else: # CASE: wHK
-        decoded_latent = lvm.vae.decode(xxyy)
-        decoded_seq = tf.one_hot(tf.argmax(decoded_latent, axis=-1), k.AA).reshape(1, decoded_latent.shape[0], k.L*k.AA)
-        decoded_reference = lvm.vae.decode(ref_point.reshape(1, 2))
-        decoded_ref_seq = tf.one_hot(tf.argmax(decoded_reference, axis=-1), k.AA).reshape(1, ref_point.shape[0], k.L*k.AA)
-        values = k(decoded_seq, decoded_ref_seq) # NOTE: weighting distribution in weighting vector, use OneHot for X
-    values = tf.squeeze(values).numpy()
-    im_size = int(np.sqrt(values.shape)) # quadratic image
-    ax.imshow(values.reshape((im_size, im_size)), vmin=vmin, vmax=vmax, cmap=cmap)
+def plotlatentspace_lvm_refpoint(k_values: np.ndarray, ax, xmin: float=-2., xmax: float=2., stepsize=0.01, ref_point=(0,0), vmin=0., vmax=1., suffix="", cmap="viridis"):
+    im_size = int(np.sqrt(k_values.shape)) # quadratic image
+    ax.imshow(k_values.reshape((im_size, im_size)), vmin=vmin, vmax=vmax, cmap=cmap)
     # ax.plot(ref_point, "x", color="darkred" markersize=25.)
     # TODO: set correct x,y labels
     # if isinstance(ref_point, tf.Tensor):
     #     ref_point = ref_point.numpy()
     ref_point = np.array(tf.squeeze(ref_point))
-    ax.set_title(f"z=[{str(np.round(ref_point[0], 2))}, {str(np.round(ref_point[1], 2))}]\n {k.__class__.__name__}", fontsize=23)
+    ax.set_title(f"z=[{str(np.round(ref_point[0], 2))}, {str(np.round(ref_point[1], 2))}]{suffix}", fontsize=23)
+
+
+def plotlatentspace_lvm_refpoint_contour(k_values: Kernel, ax, xmin: float=-2., xmax: float=2., stepsize=0.01, ref_point=(0,0), vmin=0., vmax=1., suffix="", cmap="viridis"):
+    im_size = int(np.sqrt(k_values.shape)) # quadratic image
+    cs = ax.contour(k_values.reshape((im_size, im_size)), cmap=cmap)
+    ax.clabel(cs, inline=True, fontsize=12)
+    x_range = np.arange(xmin, xmax, step=stepsize)
+    tick_labels = [f'{tick:.2f}' for tick in x_range]
+    # ax.set_xticks(x_range, tick_labels)
+    # ax.set_yticks(x_range, tick_labels)
+    ax.set_xticklabels(tick_labels)
+    ax.set_yticklabels(tick_labels) #TODO tick-labels are off fix this!
+    ax.set_xlabel(r"$z_1$", fontsize=18)
+    ax.set_ylabel(r"$z_2$", fontsize=18)
+    ax.tick_params(axis="x", labelsize=13)
+    ax.tick_params(axis="y", labelsize=13)
+    ref_point = np.array(tf.squeeze(ref_point))
+    ax.set_title(f"z=[{str(np.round(ref_point[0], 2))}, {str(np.round(ref_point[1], 2))}]{suffix}", fontsize=20)
